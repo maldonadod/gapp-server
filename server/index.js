6727ba55de4844c8b45c0918f616e6c7ef207301
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-10 15:43:03 -0300

    support port env var

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express()
 
+const PORT = process.env.PORT || 9052
+
 app.get('/', (req, res) => {
   res.send('Gapp')
 });
 
-app.listen(9052, function() {
-  console.log('Listening at 9052')
+app.listen(PORT, function() {
+  console.log(`Listening at ${PORT}`)
 })
