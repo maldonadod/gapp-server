ecce312676d3f8a907487a1b10703bcdbc8ea19c
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-10 18:49:32 -0300

    remove check for password

--- src/SignUp/business.js
+++ src/SignUp/business.js
@@ -1,16 +1,9 @@
-const confirmPassword = (p, cp) => p === cp
-
 const register = data => {
 
   const {
     username
     ,password
-    ,confirm_password
   } = data
-
-  if (!confirmPassword(password, confirm_password)) {
-    return Promise.reject('Password does not match')
-  }
 
   return Promise.resolve(data)
 }
