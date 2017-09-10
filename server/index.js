753bad9e8c5e02a0d0be1bf91afc99cada44fcc8
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-10 17:38:49 -0300

    add body parser middleware, add route /signup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json({extended: true}))
+
+const SignUp = require('../src/SignUp')
 
 const PORT = process.env.PORT || 9052
 
 app.get('/', (req, res) => {
   res.send('Gapp')
 });
+
+app.post('/signup', SignUp.post);
 
 app.listen(PORT, function() {
   console.log(`Listening at ${PORT}`)
