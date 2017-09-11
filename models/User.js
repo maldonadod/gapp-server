10263d14cc15bb2b1bbe277532a637cc76463f10
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-10 22:44:01 -0300

    User updateAccessTokenProvider

--- models/User.js
+++ models/User.js
@@ -8,4 +8,14 @@ const User = new Schema({
   collection: 'User'
 })
 
+User.methods.updateAccessTokenProvider = function(access_token) {
+  this.set({
+    provider: {
+      facebook: access_token
+    }
+  })
+
+  return this.save()
+}
+
 module.exports = mongoose.model('User', User)
