fe1207a91ccb7646ef86cd1d706a77a13770a72d
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-12 00:24:46 -0300

    add [get] profile/me endpoint

--- server/index.js
+++ server/index.js
@@ -9,14 +9,28 @@ app.use(bodyParser.json({extended: true}))
 const Sign = require('../src/SignUp')
 const Login = require('../src/LogIn')
 
+const UserProfile = require('../src/UserProfile')
+
 const PORT = process.env.PORT || 9052
 
-app.get('/', (req, res) => {
-  res.send('Gapp')
-});
+const HomeHandler = (req, res) => {
+  res.send('G ~ OnLine')
+}
+
+app.get('/', HomeHandler)
 
 app.post('/signup', Sign.post);
 app.post('/login', Login.post);
+
+// Middlewares
+
+const Auth = (req, res, next) => {
+  // logic
+
+  next()
+}
+
+app.get('/profile/me', Auth, UserProfile.get)
 
 app.listen(PORT, function() {
   console.log(`Listening at ${PORT}`)
