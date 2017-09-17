ae13a9d34f44432c746c0a32444f0e20d4af0de5
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-09-17 12:47:46 -0300

    SignUp: hash password before save

--- src/SignUp/index.js
+++ src/SignUp/index.js
@@ -2,22 +2,35 @@ const SignUpBusiness = require('./business');
 const {
   GetUserToken
 } = require('../Auth/middleware')
+const {
+  Hash
+} = require('../Auth/business')
 
 module.exports = {
   post: (req, res) => {
 
-    SignUpBusiness.register(req.body)
-    .then(GetUserToken)
-    .then(user => {
-      res.send({
-        status: 'OK',
-        data: user
+    const {password} = req.body
+
+    Hash(password)
+    .then(hashpassword => {
+
+      const input = Object.assign({}, req.body, {
+        password: hashpassword
       })
-    })
-    .catch(err => {
-      res.send({
-        status: 'ERR',
-        message: err
+
+      SignUpBusiness.register(input)
+      .then(GetUserToken)
+      .then(user => {
+        res.send({
+          status: 'OK',
+          data: user
+        })
+      })
+      .catch(err => {
+        res.send({
+          status: 'ERR',
+          message: err
+        })
       })
     })
   }
