73a4c206396e4088c2ff0478fa595f6185804035
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-01 16:26:54 -0300

    PromiseHandler module

---
+++ responses/PromiseHandler.js
@@ -0,0 +1,29 @@
+const {
+  success
+  ,error
+} = require('../responses')
+
+const {
+  parseResponse
+} = require('../src/Pagination/business')
+
+const successPagination = success => parseResponse => data => parseResponse(success(data))
+const formatAndResponse = format => res => data => res.send(format(data))
+const responseSuccessPagination = formatAndResponse(successPagination(success)(parseResponse))
+const responseSuccess = formatAndResponse(success)
+const responseError = formatAndResponse(error)
+
+const handlerPromiseFactory = success => error => getPromise => (req, res) => {
+  getPromise(req)
+  .then(success(res))
+  .catch(error(res))
+}
+
+const handlerPromise = handlerPromiseFactory(responseSuccess)(responseError)
+const handlerPromisePagination = handlerPromiseFactory(responseSuccessPagination)(responseError)
+
+module.exports = {
+  handlerPromiseFactory
+  ,handlerPromise
+  ,handlerPromisePagination
+}
