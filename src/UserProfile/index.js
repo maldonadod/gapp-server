3dfe12d14b873a4924ecb6cdb42a50a83aef4c87
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-01 16:25:55 -0300

    move PromiseHandler and selectors to modules

--- src/UserProfile/index.js
+++ src/UserProfile/index.js
@@ -1,50 +1,27 @@
 const UserProfileBussiness = require('./business')
 const UsersBusiness = require('../Users/business')
 const ChaptersBusiness = require('../Chapters/business')
-const {
-  getOptions
-  ,parseResponse
-} = require('../Pagination/business');
 
 const {
-  success
-  ,error
-} = require('../../responses')
+  getLoggedUserIdFromReq
+  ,getPaginateOptionsFromReq
+  ,getAuthorFromLoggedUser
+} = require('../../selectors')
+
+const {
+  handlerPromise
+  ,handlerPromisePagination
+} = require('../../responses/PromiseHandler')
 
 module.exports = (function() {
-
-  const getLoggedUserIdFromReq = req => ({_id: req.loggedInUser._id})
   
-  const getPaginateOptionsFromReq = ({query}) => getOptions(query)
-
-  const getAuthorFromLoggedUser = ({loggedInUser}) => ({author: loggedInUser._id})
-
-  const getUserPromise = getParams => (req, res) => {
-    return UsersBusiness.findOne(getParams(req))
-  }
-  const successPagination = success => parseResponse => data => parseResponse(success(data))
-  const formatAndResponse = format => res => data => res.send(format(data))
-  const responseSuccessPagination = formatAndResponse(successPagination(success)(parseResponse))
-  const responseSuccess = formatAndResponse(success)
-  const responseError = formatAndResponse(error)
-  
-  const handlerPromiseFactory = success => error => getPromise => (req, res) => {
-    getPromise(req)
-    .then(success(res))
-    .catch(error(res))
-  }
-  
-  const handlerPromise = handlerPromiseFactory(responseSuccess)(responseError)
-  const handlerPromisePagination = handlerPromiseFactory(responseSuccessPagination)(responseError)
+  const getUserPromise = getParams => (req, res) => UsersBusiness.findOne(getParams(req))
   
   const getMyChapterPromise = getParams => getPaginateOptions => (req, res) => {
-    const params = getParams(req)
-    const paginationOptions = getPaginateOptions(req)
-    return ChaptersBusiness.paginate(params, paginationOptions)
+    return ChaptersBusiness.paginate(getParams(req), getPaginateOptions(req))
   }
  
   // create handlers
-  
   const get = handlerPromise(getUserPromise(getLoggedUserIdFromReq))
   const getMyChapters = handlerPromisePagination(getMyChapterPromise(getAuthorFromLoggedUser)(getPaginateOptionsFromReq))
  
