da2c4114d1e7fcad2fae0f26e9b72b99a5dd2f72
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-01 23:45:14 -0300

    add test for handlerPromisePagination format response

--- responses/PromiseHandler.spec.js
+++ responses/PromiseHandler.spec.js
@@ -4,13 +4,26 @@ const {
   ,handlerPromisePagination
 } = require('./PromiseHandler')
 
-const success = jest.fn()
-const error = jest.fn()
+const {
+  parseResponse
+} = require('../src/Pagination/business')
+
+const {
+  success
+  ,error
+} = require('./index')
 
 const req = {
   body: {
     name: 'Homer Simpson'
   }
+}
+
+const paginatedCollection = {
+  docs: []
+  ,total: 0
+  ,offset: 0
+  ,limit: 10
 }
 
 const responseObjs = {
@@ -34,23 +47,38 @@ const getPromiseRejectedFactory = () => jest.fn(req => Promise.reject(req.body))
 const resFactory = (fn) => ({
   send: jest.fn(fn)
 })
- 
-test('getPromise should be called in order to get a promise', () => {
-  const getPromise = getPromiseFactory()
-  const handler = handlerPromise(getPromise)
-  const res = resFactory()
-  handler(req, res)
-  .then(() => expect(getPromise.mock.calls.length).toEqual(1))
-})
-test('res should be called once on success and response model should match', () => {
-  const handler = handlerPromise(getPromiseFactory())
-  const res = resFactory(data => expect(data).toEqual(responseObjs.success))
-  handler(req, res)
-  .then(() => expect(res.send.mock.calls.length).toEqual(1))
-})
-test('res should be called once on error and response model should match', () => {
+
+describe('handlerPromise', () => {
+  test('getPromise should be called in order to get a promise', () => {
+    const getPromise = getPromiseFactory()
+    const handler = handlerPromise(getPromise)
+    const res = resFactory()
+    handler(req, res)
+    .then(() => expect(getPromise.mock.calls.length).toEqual(1))
+  })
+  test('res should be called once on success and response model should match', () => {
+    const handler = handlerPromise(getPromiseFactory())
+    const res = resFactory(data => expect(data).toEqual(responseObjs.success))
+    handler(req, res)
+    .then(() => expect(res.send.mock.calls.length).toEqual(1))
+  })
+  test('res should be called once on error and response model should match', () => {
   const handler = handlerPromise(getPromiseRejectedFactory())
   const res = resFactory(data => expect(data).toEqual(responseObjs.error))
   handler(req, res)
   .then(() => expect(res.send.mock.calls.length).toEqual(1))
+})
+})
+
+describe('handlerPromisePagination', () => {
+  
+  test('the response match with Pagination.parseResponse', () => {
+    
+    const handler = handlerPromisePagination(getPromiseFactory())
+    const res = resFactory(data => expect(data).toEqual(parseResponse(success(paginatedCollection))))
+    const req = {
+      body: paginatedCollection
+    }
+    handler(req, res)
+  })
 })
