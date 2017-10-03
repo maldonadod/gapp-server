227a965b6fa197b62538c1ef46934fd77224811a
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-02 22:21:31 -0300

    add country to User Schema and static property DEFAULT_COUNTRY_ID

--- models/User.js
+++ models/User.js
@@ -11,12 +11,17 @@ const User = new Schema({
     email: String,
     access_token: String
   },
-  profile_picture: String
+  profile_picture: String,
+  country: {
+    type: Schema.Types.ObjectId,
+    ref: 'Country'
+  }
 }, {
   collection: 'Users'
 })
 
 User.statics.USER_UNSELECTED_FIELDS = '-__v -authentication.access_token -password'
+User.statics.DEFAULT_COUNTRY_ID = '59d2d92af5229f2947e927cb'
 
 User.plugin(mongoosePaginate)
 
