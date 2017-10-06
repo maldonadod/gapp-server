600e4790a5d484304ad26c8bfb742483ead2db74
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-06 03:10:59 -0300

    Places tests

---
+++ src/Places/Places.spec.js
@@ -0,0 +1,49 @@
+const {
+  queryPlacesFrom
+} = require('./business')
+
+const modelFactory = () => ({
+  status: 'OK',
+  results: [
+    {
+      "formatted_address": "Av. Corrientes 455, C1043AAR CABA, Argentina",
+      "geometry": {
+        "location": {
+          "lat": -34.6029208,
+          "lng": -58.3732758
+        }
+      }
+    }
+  ]
+})
+
+const success = () => ([{
+  "formatted_address": "Av. Corrientes 455, CABA, Argentina",
+  "coords": {
+    "lat": -34.6029208,
+    "lng": -58.3732758
+  }
+}])
+
+describe('Places queryPlacesFrom',  () => {
+  
+  test('should call to api', () => {
+    const api = jest.fn(query => Promise.resolve(modelFactory()))
+    queryPlacesFrom(api)('queso')
+    .then(res => {
+      expect(api.mock.calls.length).toEqual(1)
+    })
+  })
+  test('response from api should be parsed and match with the success model', () => {
+    const api_res = modelFactory()
+    const api = jest.fn(query => Promise.resolve(api_res))
+    queryPlacesFrom(api)('nana')
+    .then(res => expect(res).toEqual(success()))
+  })
+  test('query should be pass into api', () => {
+    const query = 'nana'
+    const api = jest.fn(query => Promise.resolve({}))
+    queryPlacesFrom(api)(query)
+    .then(res => expect(api.mock.calls[0][0]).toEqual(query))
+  })
+})
