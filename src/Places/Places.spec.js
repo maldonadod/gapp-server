03abfa5fc72efa69504cbb89f547b12bb293c3e9
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-07 17:44:25 -0300

    add test for format_flow

--- src/Places/Places.spec.js
+++ src/Places/Places.spec.js
@@ -1,5 +1,6 @@
 const {
   queryPlacesFrom
+  ,format_flow
 } = require('./business')
 
 const modelFactory = () => ({
@@ -15,6 +16,10 @@ const modelFactory = () => ({
       }
     }
   ]
+})
+
+const resultsFactory = (...args) => ({
+  results: [...args]
 })
 
 describe('Places queryPlacesFrom',  () => {
@@ -38,5 +43,67 @@ describe('Places queryPlacesFrom',  () => {
     expect.assertions(1)
     return queryPlacesFrom(api)(query)
     .then(res => expect(api.mock.calls[0][0]).toEqual(query))
+  })
+})
+
+describe('format_flow piece', () => {
+  
+  const get_results = incoming => incoming.results
+  const format_item = item => item
+
+  
+  test('get_results should be called once and format_item should be called as many as items', () => {
+  
+    const get_results_mock = jest.fn(get_results)
+    const format_item_mock = jest.fn(format_item)
+    
+    const input = resultsFactory(1, 2, 3)
+    const format = format_flow(get_results_mock)(format_item_mock)
+    const formatted = format(input)
+    
+    expect(get_results_mock.mock.calls.length).toEqual(1)
+    expect(format_item_mock.mock.calls.length).toEqual(3)
+  })
+  
+  test('get results and return each item', () => {
+    
+    const input = resultsFactory(1, 2, 3)
+    const format = format_flow(get_results)(format_item)
+    const formatted = format(input)
+    
+    expect(formatted).toMatchSnapshot()
+  })
+  
+  test('get results and double each item', () => {
+    
+    const format_item = item => item * 2
+    
+    const input = resultsFactory(1, 2, 3)
+    const format = format_flow(get_results)(format_item)
+    const formatted = format(input)
+    
+    expect(formatted).toMatchSnapshot()
+  })
+  
+  test('format each item pass in by get_results', () => {
+    
+    const format_item = ({title,authors,date}) => {
+      return `${title} created on ${date} was a band integrated by ${authors}`
+    }
+    
+    const input = resultsFactory({
+      title: 'The doors'
+      ,authors: 'Jim Morrison, Ray Manzarek, John Densmore and Robby Krieger'
+      ,date: '1965 July'
+    }
+    ,{
+      title: 'Led zepelin'
+      ,authors: 'Robert PlantJimmy Page, John Bonham and John Paul Jones'
+      ,date: '1968'
+    })
+    const format = format_flow(get_results)(format_item)
+    const formatted = format(input)
+    
+    expect(formatted).toMatchSnapshot()
   })
 })
