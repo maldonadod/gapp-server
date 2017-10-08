e6b0d8ec84caf35047be02d972ca6a4c47ee322c
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-08 03:09:36 -0300

    change gcm-node by fcm-push

--- gcm/index.js
+++ gcm/index.js
@@ -1,11 +1,15 @@
-const gcm = require('node-gcm');
+const FCM = require('fcm-push');
 const API_KEY = process.env.GCM_API_KEY
 
-const sender = new gcm.Sender(API_KEY)
+const sender = new FCM(API_KEY)
 
-const MessageFactory = data => new gcm.Message({ data })
+const message_factory = ({to,collapse_key,data,notification}) => ({
+  to,
+  collapse_key, 
+  notification
+})
 
-const send = message => sender.send(MessageFactory(message), { registrationTokens })
+const send = (message, registrationTokens) => sender.send(message_factory(message))
 
 module.exports = {
   send
