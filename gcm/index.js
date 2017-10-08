c226be8adf3c2cf96fffef3f29229b5e0fae081b
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-08 15:27:42 -0300

    fcm-push lib

--- gcm/index.js
+++ gcm/index.js
@@ -5,11 +5,12 @@ const sender = new FCM(API_KEY)
 
 const message_factory = ({to,collapse_key,data,notification}) => ({
   to,
-  collapse_key, 
+  collapse_key,
+  data,
   notification
 })
 
-const send = (message, registrationTokens) => sender.send(message_factory(message))
+const send = (message, regids) => sender.send(message_factory({data: message, to: regids}))
 
 module.exports = {
   send
