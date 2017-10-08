616cd716f49901896ec455da5c2f5def6cfb6aff
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-08 15:28:34 -0300

    add send and format function for notification created event

--- notifications/index.js
+++ notifications/index.js
@@ -1,8 +1,29 @@
+const {
+  send
+} = require('../gcm')
+
 const factory = () => {}
 
-const chapterCreated = () => {}
-const guestStatusChanged = () => {}
-const newMessages = () => {}
+const get_args_send = get_args => incoming => {
+  const args = get_args(incoming)
+  return send(...args)
+}
+
+const chapter_created_format = chapter => {
+  const {
+    author
+    ,guests
+  } = chapter
+  
+  return [
+    `${author.first_name} invite you`
+    ,guests.map(guest => guest.user.regid)
+  ]
+}
+
+const chapterCreated = get_args_send(chapter_created_format)
 
 module.exports = {
+  chapterCreated
+  ,chapter_created_format
 }
