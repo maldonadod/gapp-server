14d891a07e790afd3a7f76a5e515a2f82bff65c4
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-17 22:46:39 -0300

    move ChapterCreated format

--- notifications/index.js
+++ notifications/index.js
@@ -1,6 +1,9 @@
 const {
   send
 } = require('../gcm')
+const {
+  chapter_created_format
+} = require('./formats')
 
 const factory = () => {}
 
@@ -9,21 +12,10 @@ const get_args_send = get_args => incoming => {
   return send(...args)
 }
 
-const chapter_created_format = chapter => {
-  const {
-    author
-    ,guests
-  } = chapter
-  
-  return [
-    `${author.first_name} invite you`
-    ,guests.map(guest => guest.user.regid)
-  ]
-}
-
+// GCMMessageI = [ message, registerIds ]
 const chapterCreated = get_args_send(chapter_created_format)
 
 module.exports = {
-  chapterCreated
-  ,chapter_created_format
+  chapterCreated,
+  chapter_created_format
 }
