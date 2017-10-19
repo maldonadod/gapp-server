ead67d9d525c86f8bb08db9ebbee61e529efa19d
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-18 23:11:35 -0300

    index: order of middlewares

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ const morgan = require('morgan')
 server.use(morgan('dev'))
 
 // APPLY MIDDLEWARES
-server.use(ERROR_HANDLER)
 server.use(API_KEY_MIDDLEWARE)
 server.use(EXTRACT_ACCESS_TOKEN)
 server.use(VALIDATE_USER_ID_FROM_REQ)
-server.use(ERROR_HANDLER)
 
 routes.forEach(REGISTER_ROUTE)
+
+server.use(ERROR_HANDLER)
 
 // DISPATCH
 server.listen(PORT, DISPATCH_CALLBACK)
