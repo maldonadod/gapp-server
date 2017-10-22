c528f7543806be4168119368d4152a2a67441831
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-22 16:31:25 -0300

    add log_info reducer

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -4,8 +4,10 @@ const {
 
 const app = require('./app')
 const errors = require('./error')
+const log_info = require('./log_info')
 
 module.exports = combineReducers({
   app
   ,errors
+  ,log_info
 })
