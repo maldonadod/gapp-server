addf8abbb24dc0de68f689c2c038a9dde9e397a2
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-10-26 23:17:03 -0300

    update test

--- upload/upload.spec.js
+++ upload/upload.spec.js
@@ -3,7 +3,7 @@ jest.mock('cloudinary')
 const middleware = require('./middleware')
 const cloudinary = require('cloudinary')
 const {
-  chapterFormatNotification
+  chapterCoverUploadFormat
 } = require('../src/Chapters/business')
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 5
@@ -38,7 +38,7 @@ describe('Upload', () => {
       expect(res.locals).toMatchSnapshot()
       expect(next.mock.calls.length).toEqual(1)
     }))
-    
-    middleware(chapterFormatNotification)(req, res, next)
+
+    middleware(chapterCoverUploadFormat)(req, res, next)
   })
 })
